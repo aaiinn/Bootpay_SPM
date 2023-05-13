5aadde4122284d837a9e77f8ccf0bd6366db6083
Shin ain <s22028@gsm.hs.kr>
2023-05-13 20:23:33 +0900

    Change platforms

--- Package.swift
+++ Package.swift
@@ -1,26 +1,29 @@
-// swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "Bootpay_SPM",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Bootpay_SPM",
-            targets: ["Bootpay_SPM"]),
+            targets: ["Bootpay_SPM"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/bootpay/ios_swift", from: "4.4.1"),
+        .package(url: "https://github.com/bootpay/ios_swift.git", from: "4.4.1"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Bootpay_SPM"),
+            name: "Bootpay_SPM",
+            dependencies: ["Bootpay"]
+        ),
         .testTarget(
             name: "Bootpay_SPMTests",
-            dependencies: ["Bootpay_SPM"]),
+            dependencies: ["Bootpay_SPM"]
+        ),
     ]
 )
