9ff37fda5af4ce4c0ad6b9d0df3f0a21218a53d5
Shin ain <s22028@gsm.hs.kr>
2023-05-14 22:55:14 +0900

    Add Bootpay SPM

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Bootpay_SPM",
+    name: "Bootpay",
     platforms: [
         .iOS(.v10)
     ],
@@ -13,13 +13,12 @@ let package = Package(
             targets: ["Bootpay_SPM"]
         ),
     ],
-    dependencies: [
-        .package(url: "https://github.com/bootpay/ios_swift.git", from: "4.4.1"),
-    ],
+    dependencies: [],
     targets: [
         .target(
             name: "Bootpay_SPM",
-            dependencies: ["Bootpay"]
+            dependencies: [],
+            path: "Sources"
         ),
         .testTarget(
             name: "Bootpay_SPMTests",
